9ed43fbbf66d8fc97b6bae17db34c9883f932b86
afolabi <yusufafolabi95@gmail.com>
2024-06-15 19:04:33 +0100

    updated

--- controllers/seasonController.js
+++ controllers/seasonController.js
@@ -1,47 +1,49 @@
-const Season = require('../models/seasonModel')
+const Season = require('../models/seasonModel');
 
-
-const createSeason = async(req, res) =>{
-
+const createSeason = async (req, res) => {
     try {
-        let season = Season.findOne(req.body)
+        let season = await Season.findOne({ season: req.body.season });
         if (season) return res.status(400).json({ msg: 'Season already exists' });
-    
-       season = new Season(req.body);
+
+        season = new Season(req.body);
         await season.save();
-        res.status(401).json({ msg: 'Season created' })
-      } catch (err) {
+        res.status(201).json({ msg: 'Season created' });
+    } catch (err) {
         res.status(500).send('Server error');
-      }
+    }
 };
 
-const getSeason = async(req, res) =>{
-
+const getSeason = async (req, res) => {
     try {
-        const season = Season.findOne(req.body)
-       
-        res.status(401).json(season)
-      } catch (err) {
+        const season = await Season.findOne({ season: req.body.season });
+        if (!season) return res.status(404).json({ msg: 'Season not found' });
+
+        res.status(200).json(season);
+    } catch (err) {
         res.status(500).send('Server error');
-      }
+    }
 };
 
-const updateSeason = async(req, res) =>{
-
+const updateSeason = async (req, res) => {
+    const { season, newSeason } = req.body;
     try {
-        let season = Season.findOne(req.body)
-        if (!season) return res.status(400).json({ msg: 'Season not exists' });
-    
-       season = new Season.findOneAndUpdate(req.body);
-        await season.save();
-        res.status(401).json({ msg: 'Season created' })
-      } catch (err) {
+        let existSeason = await Season.findOne({ season });
+        if (!existSeason) return res.status(400).json({ msg: 'Season does not exist' });
+
+        const updatedSeason = await Season.findOneAndUpdate(
+            { season },
+            { season: newSeason },
+            { new: true }
+        );
+
+        res.status(200).json({ msg: 'Season updated', updatedSeason });
+    } catch (err) {
         res.status(500).send('Server error');
-      }
+    }
 };
 
 module.exports = {
-    createSeason, 
-    getSeason, 
+    createSeason,
+    getSeason,
     updateSeason
-}
+};
