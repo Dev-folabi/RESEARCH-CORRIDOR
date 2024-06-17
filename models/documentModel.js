a85749d3d4ddc49994d021d474ef84a20ffd8816
afolabi <yusufafolabi95@gmail.com>
2024-06-17 08:32:09 +0100

    doc model updated

--- models/documentModel.js
+++ models/documentModel.js
@@ -4,6 +4,7 @@ const DocumentSchema = new mongoose.Schema({
     researcherId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     supervisorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     document: { type: String, required: true },
+    status: {type: String, enum: ['Reviewed', 'Not Reviewed']},
     comments: [{ supervisorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, comment: String }],
     createdAt: {
       type: Date,
