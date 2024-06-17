1b7fa3c32706a6dade8bc3287f7d878e9c8f8eb4
afolabi <yusufafolabi95@gmail.com>
2024-06-17 08:35:37 +0100

    index updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path')
 
 
 require('dotenv').config()
