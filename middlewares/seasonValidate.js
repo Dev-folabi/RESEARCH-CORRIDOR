bd365d3618d7dadb4117e9e56f19351afca2adda
afolabi <yusufafolabi95@gmail.com>
2024-07-06 12:13:46 +0100

    updated

--- middlewares/seasonValidate.js
+++ middlewares/seasonValidate.js
@@ -2,7 +2,12 @@ const Season = require("../models/seasonModel");
 
 const setSeason = async (req, res, next) => {
   const { season } = req.header('season'); 
+    if (!season) {
+      return res.status(400).json({ msg: "Season is required" });
+    }
+  
   try {
+
     const seasonDoc = await Season.findOne({ season });
     if (!seasonDoc) {
       return res.status(400).json({ msg: "Season does not exist" });
@@ -12,6 +17,7 @@ const setSeason = async (req, res, next) => {
   } catch (err) {
     res.status(500).json({ msg: "Internal Server Error", error: err.message });
   }
+  
 };
 
 module.exports = setSeason;
