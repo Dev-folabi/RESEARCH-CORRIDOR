3da5ddf2821a65af361d0e3fcec3b6c6cbaed0d0
afolabi <yusufafolabi95@gmail.com>
2024-07-24 02:51:20 +0100

    Email notification updated

--- config/email.js
+++ config/email.js
@@ -3,12 +3,23 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// const transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 487,
+    secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
     },
-});
+  });
 
 module.exports = transporter;
--- utils/notifier.js
+++ utils/notifier.js
@@ -1,8 +1,24 @@
 const transporter = require('../config/email');
 require('dotenv')
+// const sendEmail = async (to, subject, text) => {
+//     const mailOptions = {
+//         from: process.env.EMAIL_USER,
+//         to,
+//         subject,
+//         text,
+//     };
+
+//     try {
+//         await transporter.sendMail(mailOptions);
+//         console.log(`Email sent to ${to}`);
+//     } catch (error) {
+//         console.error(`Error sending email to ${to}:`, error);
+//     }
+// };
+
 const sendEmail = async (to, subject, text) => {
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: '"Research Corridor" <process.env.EMAIL_USER>',
         to,
         subject,
         text,
